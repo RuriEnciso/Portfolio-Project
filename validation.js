e48420c3f7d4c749ebc9c9caf97233110364d2e1
RuriEnciso <rurienciso@gmail.com>
2023-05-23 17:51:28 -0500

    fixed linters

---
+++ validation.js
@@ -0,0 +1,5 @@
+// const form = document.querySelector('form');
+// const name = document.querySelector('#name');
+// const spanName = document.querySelector('#spanName');
+// const email = document.querySelector('#mail');
+// const spanEmail = document.querySelector('#spanEmail');
