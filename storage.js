612c3c3e92be1eed5e80c5acb4ed3bf6d046a2d4
Amaramhdi1112 <amarmahdi1112@gmail.com>
2023-05-26 15:22:56 -0600

    Add user data persistance

--- storage.js
+++ storage.js
@@ -2,33 +2,44 @@ const nameField = document.getElementById("name");
 const emailField = document.getElementById("mail");
 const messageField = document.getElementById("msj");
 
-window.onload = () => {
-    const data = localStorage.getItem("userData");
-    if (data != null){
-        const obj = JSON.parse(data);
+const ud = {
+  fullName: '',
+  email: '',
+  message: ''
+};
 
-    }
+window.onload = () => {
+  const data = localStorage.getItem("userData");
+  if (data != null) {
+    const obj = JSON.parse(data);
+    nameField.value = obj.fullName;
+    emailField.value = obj.email;
+    messageField.value = obj.message;
+    ud.fullName = obj.fullName;
+    ud.email = obj.email;
+    ud.message = obj.message;
+  }
 }
 
 const storeData = (data) => {
-    const str = JSON.stringify(data);
-    localStorage.setItem("userData", str);
-    console.log(localStorage)
+  const str = JSON.stringify(data);
+  localStorage.setItem("userData", str);
+  console.log(localStorage)
 }
 
 nameField.addEventListener("change", (event) => {
-    ud.fullName = event.target.value;
-    storeData(ud);
+  ud.fullName = event.target.value;
+  storeData(ud);
 });
 
 emailField.addEventListener("change", (event) => {
-    ud.email = event.target.value;
-    storeData(ud);
+  ud.email = event.target.value;
+  storeData(ud);
 });
 
 messageField.addEventListener("change", (event) => {
-    ud.message = event.target.value;
-    storeData(ud);
+  ud.message = event.target.value;
+  storeData(ud);
 });
 
 
