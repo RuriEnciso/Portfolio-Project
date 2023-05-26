fb1f9eab1c18e451f5e7cb038e6d9d34617a62a2
RuriEnciso <rurienciso@gmail.com>
2023-05-26 11:56:16 -0500

    Fexed validation.js to error appears when a user tries to submit the form.

--- validation.js
+++ validation.js
@@ -25,32 +25,6 @@ function showError() {
   nameError.className = 'error';
 }
 
-fullName.addEventListener('input', () => {
-  if (fullName.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    nameError.textContent = ''; // Reset the content of the message
-    nameError.className = 'error'; // Reset the visual state of the message
-  } else {
-    // If there is still an error, show the correct error
-    showError();
-  }
-});
-
-email.addEventListener('input', () => {
-  // Each time the user types something, we check if the
-  // form fields are valid.
-  if (email.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    emailError.textContent = ''; // Reset the content of the message
-    emailError.className = 'error'; // Reset the visual state of the message
-  } else {
-    // If there is still an error, show the correct error
-    showError();
-  }
-});
-
 form.addEventListener('submit', (event) => {
   // if the email field is valid, we let the form submit
   if (!email.validity.valid || !fullName.validity.valid) {
